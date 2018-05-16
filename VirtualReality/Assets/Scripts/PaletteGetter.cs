3b4c6df6e5b52e417008de83c71a8b9e1deffbf3
Sergio Sánchez López <s.sanchezl@edu.uah.es>
2018-05-16 12:47:16 +0200

    Added new shader for target customization, added font scaler script.

---
+++ VirtualReality/Assets/Scripts/PaletteGetter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Accessibility;
+
+public class PaletteGetter : MonoBehaviour {
+
+	public Color[] palette;
+	public Color[] targetColors;
+	void Awake() {
+		DontDestroyOnLoad(this);
+		VisionUtility.GetColorBlindSafePalette(palette, 0.25f, 0.75f);
+	}
+}
