94c75caa61f492812c1418086100717911a8b0d5
Sergio Sánchez López <s.sanchezl@edu.uah.es>
2018-06-05 22:27:26 +0200

    Began revamp of options menu, added all UI elements with some placeholders.

---
+++ VirtualReality/Assets/Scripts/ScoreHolderPositionCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreHolderPositionCalculator : MonoBehaviour {
+
+	[SerializeField] private Vector3 furtherPoint;
+	[SerializeField] private Vector3 closerPoint;
+
+	private float maximumScale;
+	private float minimumScale;
+	[SerializeField] private FontScaleManager fontScaler;
+
+	private float scale;
+
+	// Use this for initialization
+	void Start () {
+		// If scale is less than 1, make it 1 still. We don't want the
+		// canvas to be too far away.
+		this.scale = this.fontScaler.scale >= 1 ? this.fontScaler.scale : 1;
+		this.minimumScale = 1;
+		this.maximumScale = this.fontScaler.maximumScale;
+		this.transform.position = Vector3.Lerp(furtherPoint, closerPoint, (this.scale - minimumScale) / (maximumScale - minimumScale));
+	}
+}
